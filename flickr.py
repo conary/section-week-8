c3d54e952e85e0c5e54d8a5decec712798946142
conary <stefande@umich.edu>
2017-10-26 17:12:07 -0400

    Added some stuff

--- flickr.py
+++ flickr.py
@@ -25,12 +25,14 @@ def params_unique_combination(baseurl, params_d, private_keys=["api_key"]):
             res.append("{}-{}".format(k, params_d[k]))
     return baseurl + "_".join(res)
 
-def search_flickr(key, value):
+def search_flickr(key_dict):
     if not FLICKR_API_KEY:
         raise Exception('Flickr API Key is missing!')
 
     baseurl = "https://api.flickr.com/services/rest/"
-    
+        params_diction = key_dict
+        ##But now we'll have to check if method and format are there
+
         params_diction = {
            "method": "flickr.photos.search",
             "format": "json",
